4a5780be007b27000e2f25c855b2dcef0acd1e28
josealfredojimenezgarcia <joseajimenez07@gmail.com>
2021-08-30 19:54:52 -0500

    DOM

---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+var img = document.querySelectorAll(`img`);
+console.log(img);
+
+
+
+
+// var atributosImg = {
+//     src : "https://p4.wallpaperbetter.com/wallpaper/135/692/935/sci-fi-astronaut-jellyfish-space-hd-wallpaper-preview.jp",
+//     alt : "Imagen de un astronauta con una medusa"};
+
+// var imgSelecionar = document.getElementsByTagName('img');
+// Object.assign(imgSelecionar.imgID,atributosImg);
+
+
+
+
+
+
+
+// let atributosImg = {
+//     src : "./logo.jpg",
+//     alt : "Imagen de un astronauta en la luna tomando una cerveza"};
+// let img = document.createElement('IMG');
+// Object.assign(img, atributosImg);
+// document.body.appendChild(img);
+// console.log(img);
+
+
+
+
+
+
+
+
+// let fragmen = new DocumentFragment();
+// let h1 = document.createElement('H1');
+// let texto = document.createTextNode(`Titulo 1`);
+// h1.appendChild(texto);
+// fragmen.appendChild(h1);
+// document.body.appendChild(fragmen);
+
+
+
+
+// document.body.innerHTML = "<h1>f12</h1>";
+
+
+
